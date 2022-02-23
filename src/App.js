f8333f83982e3f6937bdbad5bce5bedac0a6f969
mastercode15 <sebastian.morales@epn.edu.ec>
2022-02-23 16:18:29 -0500

    test with custom json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    await fetch("https://my-json-server.typicode.com/mastercode15/jsondb/db")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  useEffect(() => {
+    document.dispatchEvent(
+      new CustomEvent("setToken", {
+        detail: { clientId: data?.detail?.clientId, clientidType: "0001" },
+      })
+    );
+    console.log("se disparó el evento con la ci:", data?.detail?.clientId);
+  }, [data]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>CI cliente: {data?.detail?.clientId}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
