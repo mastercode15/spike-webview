b03f01642bb49e9e1da01f991333867e237e4450
mastercode15 <sebastian.morales@epn.edu.ec>
2022-03-07 16:03:46 -0500

    final spike

--- src/App.js
+++ src/App.js
@@ -3,28 +3,13 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [data, setData] = useState({});
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(async () => {
-    await fetch("https://my-json-server.typicode.com/mastercode15/jsondb/db")
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
-
-  useEffect(() => {
-    document.dispatchEvent(
-      new CustomEvent("setToken", {
-        detail: { clientId: data?.detail?.clientId, clientidType: "0001" },
-      })
-    );
-    console.log("se disparó el evento con la ci:", data?.detail?.clientId);
-  }, [data]);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>CI cliente: {data?.detail?.clientId}</p>
+        <p>CI cliente:</p>
+        {/* eslint-disable-next-line no-undef */}
+        <p id="clientId">{variable}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
